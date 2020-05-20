ac0e2997b440abfe235773573e5a4b0d55389c89
DVGY <gaurav.gydv@gmail.com>
2020-05-20 12:54:29 +0530

    Navbar added

--- embellish/src/App.js
+++ embellish/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/ShopPage';
+import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import './App.css';
 function App() {
   return (
     <Router>
+      <Navbar />
+
       <div className="App">
         <Switch>
           <Route exact path="/" component={Homepage} />
